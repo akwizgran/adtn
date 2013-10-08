c95bfb6b6a36e4916fb036f6738645992e10cda2
akwizgran <akwizgran@users.sourceforge.net>
2013-10-08 16:01:45 +0100

    Created a placeholder activity and ant build files.

---
+++ src/net/sf/adtn/ui/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package net.sf.adtn.ui;
+
+import net.sf.adtn.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeScreenActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_screen_activity);
+	}
+}
